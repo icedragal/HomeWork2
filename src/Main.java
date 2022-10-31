bc0e1da52e496714394c4d4b9b7eeebbad6e65de
Icedragal <icedragal@gmail.com>
2022-10-31 19:54:35 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
     // Приступил к ДЗ
-
+        //Task1
+        System.out.println("Task 1");
+        var dog = 8.0;
+        var cat =3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
     }
 }
